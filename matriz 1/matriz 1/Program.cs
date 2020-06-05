b3d871f637408ead8ba73c565227665209787376
Nelly Odette Pérez Osoy <201800326@colsanjose.edu.gt>
2020-06-04 22:37:09 -0600

    creacion del proyecto

---
+++ matriz 1/matriz 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matriz_1
+{
+    class Program
+    {
+        static int mat = 3;
+        static string[,] matriz = new string[mat, mat];
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESE SUS CARACTERES\n");
+
+            for (int e = 0; e < mat; e++)
+            {
+                for (int m = 0; m < mat; m++)
+                {
+                    Console.WriteLine("\nE: " + e + "M: " + m + "\n");
+
+                    Console.Write("caracter: ");
+                    matriz[m, e] = Console.ReadLine();
+                }
+            }
+
+            Console.WriteLine("....................................\n");
+            for (int e = 0; e < mat; e++)
+            {
+                for (int m = 0; m < mat; m++)
+                {
+                    Console.WriteLine("valor; " + matriz[m, e]);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
